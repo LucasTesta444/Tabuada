3e4052c8b95ee943c02b84bbbcfd1da764622919
Lucas Testa <61668502+LucasTesta444@users.noreply.github.com>
2022-10-25 16:56:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ botao.addEventListener("click", (event) => {
     event.preventDefault()
     let n = (document.getElementById("numeroDaTabuada").value);
     if(n <=0) {
-        alert("Digite um numero maior que 0!");
+        alert("Digite um número maior que 0!");
     }else{
         saida.classList.replace("campoTabuada", "campoTabuadaON")
         c=1
